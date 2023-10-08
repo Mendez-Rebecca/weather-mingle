b6b291b5e56326b4cf2f40cc1e1f1d313b8181ed
Rebecca <mendez_elizabeth88@hotmail.com>
2023-10-07 21:26:59 -0700

    add data for weather info for a user's humidity, pressure, etc

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,5 +1,13 @@
-export default function WeatherInfo() {
+export default function WeatherInfo({ weatherData }) {
     return (
-        <h1>WeatherInfo</h1>
+        <div>
+            <h2>WeatherInfo</h2>
+            <p>Humidity: {weatherData[0]}%</p>
+            <p>Pressure: {(weatherData[1] / 33.863886666667).toFixed(2)} inHg</p>
+            <p>Visibility: {(weatherData[2]).toFixed(0)} mi</p>
+            <p>Wind Speed: {(weatherData[3] * 2.237).toFixed(0)} mph</p>
+            <p>Dew Point: {weatherData[4]}°</p>
+            <p>UV Index: {weatherData[5]}</p>
+        </div>
     )
 }
--- src/pages/WeatherPage/WeatherPage.jsx
+++ src/pages/WeatherPage/WeatherPage.jsx
@@ -7,6 +7,7 @@ const getTimelineURL = "https://api.tomorrow.io/v4/weather/forecast";
 export default function WeatherPage() {
     const [currentTemp, setCurrentTemp] = useState(null);
     const [location, setLocation] = useState({ latitude: null, longitude: null });
+    const [weatherData, setWeatherData] = useState([]);
 
     const APIKey = process.env.REACT_APP_API_KEY;
 
@@ -38,6 +39,14 @@ export default function WeatherPage() {
                 .then(response => response.json())
                 .then(data => {
                     setCurrentTemp(data.timelines.hourly[0].values.temperature);
+                    setWeatherData([
+                        data.timelines.hourly[0].values.humidity,
+                        data.timelines.hourly[0].values.pressureSurfaceLevel,
+                        data.timelines.hourly[0].values.visibility,
+                        data.timelines.hourly[0].values.windSpeed,
+                        data.timelines.hourly[0].values.dewPoint,
+                        data.timelines.hourly[0].values.uvIndex,
+                    ])
                 })
                 .catch(error => {
                     console.error("Error fetching weather data:", error);
@@ -49,7 +58,7 @@ export default function WeatherPage() {
         <div>
             <h1>Weather App</h1>
             <CurrentTemp currentTemp={currentTemp} />
-            <WeatherInfo />
+            <WeatherInfo weatherData={weatherData} />
         </div>
     );
 };
