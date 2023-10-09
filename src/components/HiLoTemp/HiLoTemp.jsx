addcf04aa6844d5330904253c8be9b8630e02df7
Rebecca <mendez_elizabeth88@hotmail.com>
2023-10-08 17:28:40 -0700

    styling done for hi and lo temp

--- src/components/HiLoTemp/HiLoTemp.jsx
+++ src/components/HiLoTemp/HiLoTemp.jsx
@@ -1,8 +1,14 @@
 export default function HiLoTemp({ temps }) {
     return (
-        <div>
-            <p>Hi: {(temps[21] * 9 / 5 + 32).toFixed(0)}°F</p>
-            <p>Lo: {(temps[22] * 9 / 5 + 32).toFixed(0)}°F</p>
+        <div className="TempContainer">
+            <div className="HiTemp">
+                <h2>Hi</h2>
+                <p>{(temps[21] * 9 / 5 + 32).toFixed(0)}°F</p>
+            </div>
+            <div className="LoTemp">
+                <h2>Lo</h2>
+                <p>{(temps[22] * 9 / 5 + 32).toFixed(0)}°F</p>
+            </div>
         </div>
     )
 }
