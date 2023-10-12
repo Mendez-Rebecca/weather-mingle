4c9d0114cb0fb63181ff2ac70ab3f60afae889ef
Rebecca <mendez_elizabeth88@hotmail.com>
2023-10-12 09:42:33 -0700

    style the chat button to match color scheme based on weather condition

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -44,62 +44,74 @@ export default function CurrentWeather({ currentWeather }) {
         let backgroundImage;
         let backgroundColor;
         let windowColor;
+        let buttonColor;
 
         switch (weatherCondition) {
             case 'Sunny':
                 backgroundImage = `url(${sunnyBackground})`;
                 backgroundColor = '#EAB269';
                 windowColor = 'rgba(154, 146, 110, 0.7)';
+                buttonColor = '#EAB269';
                 break;
             case 'Mostly Clear':
                 backgroundImage = `url(${mostlyClearBackground})`;
                 backgroundColor = '#8F9FA6';
                 windowColor = 'rgb(143, 159, 166, 0.7)';
+                buttonColor = '#8F9FA6';
                 break;
             case 'Partly Cloudy':
                 backgroundImage = `url(${partlyCloudyBackground})`;
                 backgroundColor = '#B7B5B2';
                 windowColor = 'rgb(131, 130, 128, 0.7)';
+                buttonColor = '#B7B5B2';
                 break;
             case 'Mostly Cloudy':
                 backgroundImage = `url(${mostlyCloudyBackground})`;
                 backgroundColor = '#55666E';
                 windowColor = 'rgb(85, 102, 110, 0.7)';
+                buttonColor = '#55666E';
                 break;
             case 'Cloudy':
                 backgroundImage = `url(${cloudyBackground})`;
                 backgroundColor = '#364247';
                 windowColor = 'rgb(54, 66, 71, 0.7)';
+                buttonColor = '#364247';
                 break;
             case 'Thunderstorms':
                 backgroundImage = `url(${thunderstormsBackground})`;
                 backgroundColor = '#8a63a5'
                 windowColor = 'rgba(124, 110, 154, 0.7)';
+                buttonColor = '#8a63a5';
                 break;
             case 'Rain':
                 backgroundImage = `url(${rainBackground})`;
                 backgroundColor = '#788584';
                 windowColor = 'rgb(120, 133, 132, 0.7)';
+                buttonColor = '#788584';
                 break;
             case 'Snow':
                 backgroundImage = `url(${snowBackground})`;
                 backgroundColor = '#364247';
                 windowColor = 'rgb(34, 30, 32, 0.7)';
+                buttonColor = '#364247';
                 break;
             case 'Fog':
                 backgroundImage = `url(${fogBackground})`;
                 backgroundColor = '#899189';
                 windowColor = 'rgb(137, 145, 137, 0.7)';
+                buttonColor = '#899189';
                 break;
             case 'Freezing Rain':
                 backgroundImage = `url(${freezingRainBackground})`;
                 backgroundColor = '#9393A5';
                 windowColor = 'rgb(147, 147, 165, 0.7)';
+                buttonColor = '#9393A5';
                 break;
             case 'Hail':
                 backgroundImage = `url(${hailBackground})`;
                 backgroundColor = '#536512';
                 windowColor = 'rgb(83, 101, 18, 0.7)';
+                buttonColor = '#536512';
                 break;
             default:
                 backgroundImage = `url(${rainBackground})`;
@@ -108,6 +120,7 @@ export default function CurrentWeather({ currentWeather }) {
         const backgroundContainer = document.querySelector('.BackgroundContainer');
         const forecastContainer = document.querySelector('.ForecastContainer');
         const weatherInfoContainer = document.querySelector('.WeatherInfoContainer');
+        const chatButton = document.querySelector('.messageInput button');
 
         if (backgroundContainer) {
             backgroundContainer.style.backgroundImage = backgroundImage;
@@ -122,6 +135,10 @@ export default function CurrentWeather({ currentWeather }) {
         if (forecastContainer && weatherInfoContainer) {
             forecastContainer.style.backgroundColor = windowColor;
             weatherInfoContainer.style.backgroundColor = windowColor;
+        }
+
+        if (chatButton) {
+            chatButton.style.backgroundColor = buttonColor;
         }
         document.body.style.backgroundColor = backgroundColor;
     }, [weatherCondition]);
